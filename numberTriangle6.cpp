ea94dd0cbb3abddcb088c5b5222778b1ebdbb1db
SaranshBangar <saranshbangad@gmail.com>
2023-09-30 18:42:37 +0530

    Number Triangle Pattern

---
+++ numberTriangle6.cpp
@@ -0,0 +1,28 @@
+// Sample Output
+
+// Enter no. of rows : 5
+// 5 
+// 5 4 
+// 5 4 3 
+// 5 4 3 2 
+// 5 4 3 2 1 
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int row;
+
+    cout<<"Enter no. of rows : ";
+    cin>>row;
+
+    for (int i=row;i>=1;i--)
+    {
+        for (int j=row;j>=i;j--)
+        {
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
