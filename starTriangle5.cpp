eb72f659233e5535d2efb026bd395cc09f3e276f
Saransh Bangar <saranshbangad@gmail.com>
2023-10-02 18:24:25 +0530

    Star Triangle Pattern Added

---
+++ starTriangle5.cpp
@@ -0,0 +1,39 @@
+// Sample Output
+
+// Enter no. of rows : 5
+// *********
+//  *******
+//   *****
+//    ***
+//     *
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int row;
+
+    cout<<"Enter no. of rows : ";
+    cin>>row;
+
+    for (int i=0;i<=row;i++)
+    {
+        for (int j=1;j<=i;j++)
+        {
+            cout<<" ";
+        }
+
+        for (int j=row-i;j>=1;j--)
+        {
+            cout<<"*";
+        }
+        
+        for (int j=row-i-1;j>=1;j--)
+        {
+            cout<<"*";
+        }
+
+        cout<<endl;
+    }
+    return 0;
+}
