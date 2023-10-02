22b043454aed8d57c36e4a25c25d84d3abd0e092
Saransh Bangar <saranshbangad@gmail.com>
2023-10-02 18:07:20 +0530

    Number Triangle Pattern added

---
+++ numberTriangle10.cpp
@@ -0,0 +1,37 @@
+// Sample Output
+
+// Enter no. of rows : 5
+//     1
+//    121
+//   12321
+//  1234321
+// 123454321
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int row;
+
+    cout<<"Enter no. of rows : ";
+    cin>>row;
+
+    for (int i=1;i<=row;i++)
+    {
+        for (int j=row-i;j>=1;j--)
+        {
+            cout<<" ";
+        }
+        for (int j=1;j<=i;j++)
+        {
+            cout<<j;
+        }
+        for (int j=i-1;j>=1;j--)
+        {
+            cout<<j;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
